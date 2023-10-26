d1990ad36982d9f7cef6eb4163b54d332c2c01c0
RiP3rQ <rafalpompa2000@gmail.com>
2023-10-26 18:01:27 +0200

    user typos

--- core/views.py
+++ core/views.py
@@ -14,22 +14,22 @@ def loginPage(request):
     page = 'login'
 
     if request.user.is_authenticated:
-        return redirect('home')
+        return redirect('core:home')
 
     if request.method == 'POST':
-        email = request.POST.get('email').lower()
+        username = request.POST.get('username')
         password = request.POST.get('password')
 
         try:
-            user = User.objects.get(email=email)
+            user = User.objects.get(username=username)
         except:
             messages.error(request, 'User does not exist')
 
-        user = authenticate(request, email=email, password=password)
+        user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('core:home')
         else:
             messages.error(request, 'Username OR password does not exit')
 
